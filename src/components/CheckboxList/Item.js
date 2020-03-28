47e9586a7f6f37b4ca73fe4b0c05df83ab2c2b46
alexander matiugin <alexander.matiugin@pandadoc.com>
2020-03-28 13:30:37 +0300

    add Title component

--- src/components/CheckboxList/Item.js
+++ src/components/CheckboxList/Item.js
@@ -1,24 +1,19 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Checkbox } from 'antd';
 
-const Item = React.memo(({ id, name, checked, disabledList, onCheckToggle }) => {
-  const disabled = !checked && disabledList;
-  const onCheckToggleMemoized = useCallback(() => { onCheckToggle(id) }, [id]);
-
-  return (
-    <div style={{ paddingBottom: '5px' }}>
-      <Checkbox
-        disabled={disabled}
-        checked={checked}
-        onChange={onCheckToggleMemoized}
-        style={{ width: '100%'}}
-      >
-        {name}
-      </Checkbox>
-    </div>
-  )
-});
+const Item = React.memo(({ id, name, checked, disabledList, onCheckToggle }) => (
+  <div style={{ paddingBottom: '5px' }}>
+    <Checkbox
+      disabled={!checked && disabledList}
+      checked={checked}
+      onChange={() => onCheckToggle(id)}
+      style={{ width: '100%'}}
+    >
+      {name}
+    </Checkbox>
+  </div>
+));
 
 Item.propTypes = {
   onCheckToggle: PropTypes.func.isRequired,
--- src/components/TagsList/Item.js
+++ src/components/TagsList/Item.js
@@ -1,20 +1,16 @@
-import React, { useCallback } from 'react';
+import React  from 'react';
 import PropTypes from 'prop-types';
 import { Tag } from 'antd';
 
-const Item = React.memo(({ id, name, onCheckToggle }) => {
-  const onCheckToggleMemoized = useCallback(() => onCheckToggle(id),[id]);
-
-  return (
-    <Tag
-      closable
-      onClose={onCheckToggleMemoized}
-      key={id}
-    >
-      {name}
-    </Tag>
-  )
-});
+const Item = React.memo(({ id, name, onCheckToggle }) => (
+  <Tag
+    closable
+    onClose={() => onCheckToggle(id)}
+    key={id}
+  >
+    {name}
+  </Tag>
+));
 
 Item.propTypes = {
   onCheckToggle: PropTypes.func.isRequired,
